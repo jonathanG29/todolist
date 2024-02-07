66449af555c90e67d70a6e81156e2437ac8aa37b
jonathan gallego <jonathan.gallego7@gmail.com>
2024-02-07 13:40:36 -0500

    add persistence of data

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -16,15 +16,21 @@ const state =  {
 }
 
 const initStore = ()=>{
-    console.log(state);
+    loadStore();
     console.log('InitStore 🥑');
 }
 
 const loadStore = () =>{
-    throw new Error('Not implemented');
+    if( !localStorage.getItem('state')) return;
+
+    const { todos = [], filter = Filters.All} = JSON.parse( localStorage.getItem('state'));
+    state.todos = todos;
+    state.filter = filter;
 }
 
-
+const saveStateToLocalStorage = () => {
+    localStorage.setItem('state', JSON.stringify(state))
+}
 
 const getTodos = ( filter = Filters.All ) => {
     switch(filter){
@@ -48,6 +54,8 @@ const addTodo = ( description ) =>{
     if ( !description ) throw new Error ('Description is required')
 
     state.todos.push( new Todo(description))
+
+    saveStateToLocalStorage();
 }
 
 const toggleTodo = ( todoId ) => {                
@@ -58,18 +66,23 @@ const toggleTodo = ( todoId ) => {
         }
         return todo;
     })
+
+    saveStateToLocalStorage();
 }
 
 const deleteTodo = ( todoId ) => {
     state.todos = state.todos.filter( todo => todo.id !==todoId)
+    saveStateToLocalStorage();
 }
 
 const deleteCompleted = ( todoId ) => {
     state.todos = state.todos.filter( todo => todo.done)
+    saveStateToLocalStorage();
 }
 
 const setFilte = (newFilter = Filters.All) =>{
     state.filter = newFilter;
+    saveStateToLocalStorage();
 }
 
 const getCurrentFilter = () =>{
